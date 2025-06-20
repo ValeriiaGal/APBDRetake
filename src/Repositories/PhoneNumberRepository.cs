4ae99b4a969fdda08859016056ed4d9e01fc63c1
ValeriiaGal <lera.galych@gmail.com>
2025-06-20 17:05:31 +0200

    Completed post endpoint

--- src/Repositories/PhoneNumberRepository.cs
+++ src/Repositories/PhoneNumberRepository.cs
@@ -64,8 +64,77 @@ public class PhoneNumberRepository(string connectionString) : IPhoneNumberReposi
         return result;
     }
 
-    public Task<int> CreatePhoneNumberAsync(CreatePhoneNumberDTO phoneNumber)
+    public async Task<int> CreatePhoneNumberAsync(CreatePhoneNumberDTO phoneNumber)
     {
+        using var connection = new SqlConnection(connectionString);
+    await connection.OpenAsync();
+
+    using var transaction = await connection.BeginTransactionAsync();
+
+    try
+    {
+        var getOperatorCmd = new SqlCommand(
+            "SELECT Id FROM Operator WHERE Name = @Name",
+            connection, (SqlTransaction)transaction);
+
+        getOperatorCmd.Parameters.AddWithValue("@Name", phoneNumber.Operator);
+
+        var operatorIdObj = await getOperatorCmd.ExecuteScalarAsync();
+        if (operatorIdObj == null)
+            throw new ArgumentException("Operator not found.");
+
+        int operatorId = (int)operatorIdObj;
         
+        var getClientCmd = new SqlCommand(@"
+            SELECT Id FROM Client WHERE Email = @Email",
+            connection, (SqlTransaction)transaction);
+
+        getClientCmd.Parameters.AddWithValue("@Email", phoneNumber.Client.Email);
+
+        object? clientIdObj = await getClientCmd.ExecuteScalarAsync();
+
+        int clientId;
+
+        if (clientIdObj != null)
+        {
+            clientId = (int)clientIdObj;
+        }
+        else
+        {
+            var insertClientCmd = new SqlCommand(@"
+                INSERT INTO Client (Fullname, Email, City)
+                VALUES (@Fullname, @Email, @City);
+                SELECT SCOPE_IDENTITY();",
+                connection, (SqlTransaction)transaction);
+
+            insertClientCmd.Parameters.AddWithValue("@Fullname", (object?)phoneNumber.Client.FullName ?? DBNull.Value);
+            insertClientCmd.Parameters.AddWithValue("@Email", phoneNumber.Client.Email);
+            insertClientCmd.Parameters.AddWithValue("@City", (object?)phoneNumber.Client.City ?? DBNull.Value);
+
+            var insertedClientId = await insertClientCmd.ExecuteScalarAsync();
+            clientId = Convert.ToInt32(insertedClientId);
+        }
+        
+        var insertPhoneCmd = new SqlCommand(@"
+            INSERT INTO PhoneNumber (Number, Operator_Id, Client_Id)
+            VALUES (@Number, @OperatorId, @ClientId);
+            SELECT SCOPE_IDENTITY();",
+            connection, (SqlTransaction)transaction);
+
+        insertPhoneCmd.Parameters.AddWithValue("@Number", phoneNumber.MobileNumber);
+        insertPhoneCmd.Parameters.AddWithValue("@OperatorId", operatorId);
+        insertPhoneCmd.Parameters.AddWithValue("@ClientId", clientId);
+
+        var phoneIdObj = await insertPhoneCmd.ExecuteScalarAsync();
+        int phoneNumberId = Convert.ToInt32(phoneIdObj);
+
+        await transaction.CommitAsync();
+        return phoneNumberId;
+    }
+    catch
+    {
+        await transaction.RollbackAsync();
+        throw;
+    }
     }
 }
