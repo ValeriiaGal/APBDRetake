86102fd64823da225bcffeedee80d529a6836a88
ValeriiaGal <lera.galych@gmail.com>
2025-06-20 17:09:45 +0200

    fix

--- src/Models/DTOs/CreatePhoneNumberDTO.cs
+++ src/Models/DTOs/CreatePhoneNumberDTO.cs
@@ -4,9 +4,9 @@ namespace Models.DTOs;
 
 public class CreatePhoneNumberDTO
 {
-    public string MobileNumber { get; set; }
-    
     public string Operator { get; set; }
-    
+    public string MobileNumber { get; set; }
+
+
     public ClientInputDTO Client { get; set; }
 }
